9ba89dc1078b2c1eec02d80c019940c328ec0b7a
Andy Soell <andy@teamsoell.com>
2015-06-27 22:28:04 -0400

    Inclusion of AFOAuth2Manager via CocoaPods

---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'rrrrradio' do
+  pod 'AFOAuth2Manager', '2.0.0'
+end
+
---
+++ Pods/Target Support Files/Pods-rrrrradio-AFOAuth2Manager/Pods-rrrrradio-AFOAuth2Manager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rrrrradio_AFOAuth2Manager : NSObject
+@end
+@implementation PodsDummy_Pods_rrrrradio_AFOAuth2Manager
+@end
